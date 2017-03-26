ce8810f7517c2bede2daefafab8e2096613c310d
Lily Hoskin <lilyhoskin@gmail.com>
2017-03-26 16:49:26 +0100

    Add redux dependancies and add eslint

--- index.android.js
+++ index.android.js
@@ -1,53 +1,53 @@
 /**
- * Sample React Native App
- * https://github.com/facebook/react-native
- * @flow
- */
+* Sample React Native App
+* https://github.com/facebook/react-native
+* @flow
+*/
 
-import React, { Component } from 'react';
+import { Component } from "react"
 import {
   AppRegistry,
   StyleSheet,
   Text,
   View
-} from 'react-native';
+} from "react-native"
 
 export default class CandleLight extends Component {
   render() {
     return (
       <View style={styles.container}>
         <Text style={styles.welcome}>
-          Welcome to React Native! 😀
+        Welcome to React Native! 😀
         </Text>
         <Text style={styles.instructions}>
-          To get started, edit index.android.js 
+        To get started, edit index.android.js 
         </Text>
         <Text style={styles.instructions}>
-          Double tap R on your keyboard to reload,{'\n'}
-          Shake or press menu button for dev menu
+        Double tap R on your keyboard to reload,{"\n"}
+        Shake or press menu button for dev menu
         </Text>
       </View>
-    );
+    )
   }
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#F5FCFF",
   },
   welcome: {
     fontSize: 20,
-    textAlign: 'center',
+    textAlign: "center",
     margin: 10,
   },
   instructions: {
-    textAlign: 'center',
-    color: '#333333',
+    textAlign: "center",
+    color: "#333333",
     marginBottom: 5,
   },
-});
+})
 
-AppRegistry.registerComponent('CandleLight', () => CandleLight);
+AppRegistry.registerComponent("CandleLight", () => CandleLight)
--- index.ios.js
+++ index.ios.js
@@ -4,13 +4,13 @@
  * @flow
  */
 
-import React, { Component } from 'react';
+import { Component } from "react"
 import {
   AppRegistry,
   StyleSheet,
   Text,
   View
-} from 'react-native';
+} from "react-native"
 
 export default class CandleLight extends Component {
   render() {
@@ -23,31 +23,31 @@ export default class CandleLight extends Component {
           To get started, edit index.ios.js
         </Text>
         <Text style={styles.instructions}>
-          Press Cmd+R to reload,{'\n'}
+          Press Cmd+R to reload,{"\n"}
           Cmd+D or shake for dev menu
         </Text>
       </View>
-    );
+    )
   }
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#F5FCFF",
   },
   welcome: {
     fontSize: 20,
-    textAlign: 'center',
+    textAlign: "center",
     margin: 10,
   },
   instructions: {
-    textAlign: 'center',
-    color: '#333333',
+    textAlign: "center",
+    color: "#333333",
     marginBottom: 5,
   },
-});
+})
 
-AppRegistry.registerComponent('CandleLight', () => CandleLight);
+AppRegistry.registerComponent("CandleLight", () => CandleLight)
