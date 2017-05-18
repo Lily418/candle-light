994e377cc959c5665bb90b6f4c6b0adb725451c2
Lily Hoskin <lilyhoskin@gmail.com>
2017-05-18 13:07:46 +0100

    Use react-navigation package in favour of Navigator
    This fixes many of the navigation issues
    
    Close #6
    Close #8
    Close #10
    Close #14

--- src/containers/app.js
+++ src/containers/app.js
@@ -1,9 +1,11 @@
 import React from 'react'
-import { Navigator, BackAndroid } from 'react-native'
+import { BackAndroid } from 'react-native'
 import { createStore, applyMiddleware, combineReducers } from 'redux'
 import { Provider } from 'react-redux'
 import thunk from 'redux-thunk'
 import * as reducers from '../modules'
+
+import { StackNavigator, TabNavigator } from 'react-navigation';
 
 import AddFeeling from './AddFeelingContainer'
 import DescribeFeeling from './DescribeFeelingContainer'
@@ -13,50 +15,41 @@ const createStoreWithMiddleware = applyMiddleware(thunk)(createStore)
 const reducer = combineReducers(reducers)
 const store = createStoreWithMiddleware(reducer)
 
-const getRouteComponent = (route) => {
-  switch(route.key) {
-      case 'AddFeeling': return AddFeeling
-      case 'DescribeFeeling': return DescribeFeeling
-      case 'Diary': return Diary
+const MainTabs = TabNavigator({
+  Add: {
+    screen: AddFeeling,
+  },
+  Diary: {
+    screen: Diary
   }
-}
+}, {
+  tabBarPosition: 'bottom',
+  tabBarOptions: {
+    activeTintColor: '#1a8299',
+    inactiveTintColor: 'black',
+    showIcon: true,
+    style: {
+      backgroundColor: 'white'
+    },
+    indicatorStyle: {
+      backgroundColor: 'white'
+    }
+  },
+})
+
+const MainStack = StackNavigator({
+  Home: { screen: MainTabs },
+  DescribeFeeling: { screen : DescribeFeeling }
+}, {
+  headerMode: "none"
+});
+
 
 export default class App extends React.Component {
-
-
-  constructor(props) {
-  super(props)
-  this.navigator = null;
-  }
-
-  handleBack() {
-    if (this.navigator && this.navigator.getCurrentRoutes().length > 1){
-      this.navigator.pop();
-      return true; //avoid closing the app
-    } 
-
-    return false; //close the app
-  }
-  
-  componentDidMount() {
-    BackAndroid.addEventListener('hardwareBackPress', this.handleBack.bind(this));
-  }
-
-  componentWillUnmount() {
-    BackAndroid.removeEventListener('hardwareBackPress', this.handleBack);
-  }
-
   render() {
     return (
       <Provider store={store}>
-        <Navigator
-          ref={navigator => {this.navigator = navigator}}
-          initialRoute={{key: 'AddFeeling'}}
-          renderScene={(route, navigator) => {
-            const RouteComponent = getRouteComponent(route)
-            return(<RouteComponent navigator={navigator} route={route} />)
-          }}
-        />
+        <MainStack />
       </Provider>
     )
   }
