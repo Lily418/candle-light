66a2c8cbdc66f10b17d1363789380948f4e1c16f
Lily Hoskin <Lily418@users.noreply.github.com>
2017-11-12 21:28:35 +0000

    Remove unused import BackAndroid

--- src/containers/app.js
+++ src/containers/app.js
@@ -1,5 +1,4 @@
 import React from 'react'
-import { BackAndroid } from 'react-native'
 import { createStore, applyMiddleware, combineReducers } from 'redux'
 import { Provider } from 'react-redux'
 import thunk from 'redux-thunk'
